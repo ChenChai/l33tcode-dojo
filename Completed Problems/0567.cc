bf847b893a2bc875e51251b1f771e7d27ef3d266
Chen Chai <chen.chai@outlook.com>
2022-02-10 20:32:20 -0500

    Update 0567.cc

--- Completed Problems/0567.cc
+++ Completed Problems/0567.cc
@@ -1,40 +1,36 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        // target string hashset
-        vector<int> target(26, 0);
-        vector<int> current(26, 0);
         
-        // construct target
-        for (const char& c : s1) {
-            target[c - 'a']++;
+        vector<int> charsSeen(26, 0);
+        vector<int> charsWanted(26, 0);
+        
+        // Fill up charsWanted
+        for (char s : s1) {
+            charsWanted[s - 'a']++;
         }
         
         int windowStart = 0;
-        
-        // search string for permutation substring
         for (int i = 0; i < s2.size(); i++) {
-
+                        
             char c = s2[i];
-            current[c - 'a']++;
-            
-            // if char not a part of permutation, clear out
-            // until it is
-            while (windowStart <= i && current[c - 'a'] > target[c - 'a']) {
-                current[s2[windowStart] - 'a']--;
+            charsSeen[c - 'a']++;
+
+            // Next character makes permutation impossible
+            // shift window up until permutation possible
+            while (charsSeen[c - 'a'] > charsWanted[c - 'a']) {
+                
+                charsSeen[s2[windowStart] - 'a']--;
                 windowStart++;
             }
             
-            if (i - windowStart + 1 == s1.size() && target == current) { return true; }
+            
+            if (charsSeen == charsWanted) {
+                return true;
+            }
         }
         
+        
         return false;
-    }
-    
-private:
-    void clear(vector<int>& v) {
-        for (int& num : v) {
-            num = 0;
-        }
     }
 };
